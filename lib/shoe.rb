0d5e0198ac103634047144ff8e76d36fd06aff3e
tolentinoel <rckrleyn@gmail.com>
2020-10-06 18:38:53 -0700

    all green

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :material, :condition, :size, :color, :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # # def brand
+    #     @brand
+    # end
+
+    # def color
+    #     @color
+    # end
+
+    # def color=(color)
+    #     @color
+    # end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+
+    end
+
+end
+
